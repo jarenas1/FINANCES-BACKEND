40b7cee7b337116be2eb96bbe3e07cd69dc890ac
Juan Arenas <juan.arenas@bord.co>
2026-05-08 17:29:03 -0500

    refactor: db conection implementation

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,31 @@
 const { Sequelize } = require('sequelize');
 
-const dialect = process.env.DB_DIALECT || 'sqlite';
+const dialect = process.env.DB_DIALECT || 'postgres';
 
-let sequelize;
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect,
+    logging: false,
 
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST || 'localhost',
-      dialect,
-      logging: false,
-      pool: { max: 5, min: 0, acquire: 30000, idle: 10000 }
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
     }
-  );
+  }
+);
 
 module.exports = sequelize;
